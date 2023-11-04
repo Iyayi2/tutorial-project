2a6fa26d6e5fb5240a14c4be7e74ad6ade6e2ae7
Iyayi Roland <iyayiroland@gmail.com>
2023-11-04 01:35:52 +0100

    add props, hooks, custom events and event modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
   <h1>{{title}}</h1>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+  <p>Welcome...</p>
+  <button @click.alt="toggleModal">open modal</button>
 </template>
 
 <script>
-
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
+  components: { Modal },
   data () {
     return {
-      title: 'My first vue App :)'
+      title: 'My first vue App :)',
+      header: 'Sign up for more datails',
+      text: 'Exciting paths awaits you!',
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
     }
   }
 }
@@ -23,5 +37,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
